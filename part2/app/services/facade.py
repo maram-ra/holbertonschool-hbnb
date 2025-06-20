76bb3991d1f8abc401015929a5f6aaef391a1013
KhuloodGhazi <khulood.gmo@gmail.com>
2025-06-20 16:53:14 +0000

    facade.py

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -71,20 +71,20 @@ class HBnBFacade:
         return [serialize(user) for user in self.user_repo.all()]
         
     def create_amenity(self, amenity_data):
-    # Placeholder for logic to create an amenity
-    pass
-    
+        # Placeholder for logic to create an amenity
+        pass
+
     def get_amenity(self, amenity_id):
-    # Placeholder for logic to retrieve an amenity by ID
-    pass
+        # Placeholder for logic to retrieve an amenity by ID
+        pass
     
     def get_all_amenities(self):
-    # Placeholder for logic to retrieve all amenities
-    pass
+        # Placeholder for logic to retrieve all amenities
+        pass
     
     def update_amenity(self, amenity_id, amenity_data):
-    # Placeholder for logic to update an amenity
-    pass
+        # Placeholder for logic to update an amenity
+        pass
 
 # Instantiate the facade
 facade = HBnBFacade()
