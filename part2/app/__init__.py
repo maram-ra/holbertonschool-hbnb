b522b6acac28ecc23493c26af62703e3d1e4cabf
Khulood AlOtaibi <52895786+KhuloodGhazi@users.noreply.github.com>
2025-06-29 17:03:12 +0300

    Add app.config

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -4,8 +4,9 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 
-def create_app():
+def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
+    app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     # Placeholder for API namespaces (endpoints will be added later)
