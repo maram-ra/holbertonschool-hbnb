8149711e4811698a82ec75eba02af649da4027a0
Khulood AlOtaibi <52895786+KhuloodGhazi@users.noreply.github.com>
2025-06-30 17:07:54 +0300

    Update the User Model to Include Password Hashing

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,5 +1,6 @@
 from app.models.base_model import BaseModel
 import re
+from app import bcrypt
 
 class User(BaseModel):
     def __init__(self, first_name, last_name, email, is_admin=False):
@@ -16,3 +17,11 @@ class User(BaseModel):
         self.last_name = last_name
         self.email = email
         self.is_admin = is_admin
+        
+    def hash_password(self, password):
+        """Hashes the password before storing it."""
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+        
+    def verify_password(self, password):
+        """Verifies if the provided password matches the hashed password."""
+        return bcrypt.check_password_hash(self.password, password)
