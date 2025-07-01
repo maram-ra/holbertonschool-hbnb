e84639c3c830b2ab350f8af1ffe99559ef3154b7
KhuloodGhazi <khulood.gmo@gmail.com>
2025-07-01 15:10:21 +0000

    fix

--- part3-1/app/__init__.py
+++ part3-1/app/__init__.py
@@ -6,6 +6,7 @@ from flask_jwt_extended import JWTManager
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
+from app.api.v1.auth import api as auth_ns 
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
@@ -23,4 +24,5 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
     return app
