a5aa5caa128ea940f4c74202d2f84d9e8ed5b406
KhuloodGhazi <khulood.gmo@gmail.com>
2025-07-01 14:54:51 +0000

    Configure JWT in the Flask Application

--- part3-1/app/__init__.py
+++ part3-1/app/__init__.py
@@ -1,18 +1,21 @@
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
     bcrypt.init_app(app)
+    jwt.init_app(app)
 
     # Placeholder for API namespaces (endpoints will be added later)
     # Register the users namespace
