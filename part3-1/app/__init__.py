ae383132a3f68bee0f1e0bf425cf2d7aa4051b68
KhuloodGhazi <khulood.gmo@gmail.com>
2025-07-02 19:25:13 +0000

    __init__.py

--- part3-1/app/__init__.py
+++ part3-1/app/__init__.py
@@ -3,7 +3,8 @@ from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 
-from app.api.v1.users import api as users_ns, admin_ns
+from app.api.v1.users import api as users_ns
+from app.api.v1.admin import admin_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.auth import api as auth_ns 
@@ -21,7 +22,8 @@ def create_app(config_class="config.DevelopmentConfig"):
     # Placeholder for API namespaces (endpoints will be added later)
     # Register the users namespace
     
-    api.add_namespace(users_ns, path='/api/v1/users')
+    api_v1.add_namespace(users_ns, path='/api/v1/users')
+    api_v1.add_namespace(admin_ns, path='/api/v1/users')  
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(auth_ns, path='/api/v1/auth')
